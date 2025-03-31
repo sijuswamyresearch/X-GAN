58b5686ee2708f77d6505427ced4148e8ab26df1
sijuswamyresearch <sijuswamy.research@gmail.com>
2025-04-01 02:07:42 +0530

    Update generator.py

--- models/generator.py
+++ models/generator.py
@@ -1,43 +1,28 @@
 import tensorflow as tf
 from tensorflow.keras import layers, Model
-from models.layers import EdgeAttention
+from .layers import SpectralNormalization
 
-class Generator(Model):
-    def __init__(self, img_size=256):
+class Discriminator(Model):
+    def __init__(self, config):
         super().__init__()
-        self.img_size = img_size
-        self.build_model()
+        self.config = config
+        self._build_model()
+
+    def _build_model(self):
+        inputs = layers.Input((self.config['model']['img_size'], self.config['model']['img_size'], self.config['model']['channels']))
         
-    def build_model(self):
-        inputs = layers.Input((self.img_size, self.img_size, 1))
+        x = SpectralNormalization(layers.Conv2D(64, 4, strides=2, padding='same'))(inputs)
+        x = layers.LeakyReLU(0.2)(x)
         
-        # Encoder
-        d1 = layers.Conv2D(64, 4, strides=2, padding='same')(inputs)
-        d1 = layers.LeakyReLU(0.2)(d1)
+        x = SpectralNormalization(layers.Conv2D(128, 4, strides=2, padding='same'))(x)
+        x = layers.LeakyReLU(0.2)(x)
         
-        d2 = layers.Conv2D(128, 4, strides=2, padding='same')(d1)
-        d2 = layers.BatchNormalization()(d2)
-        d2 = layers.LeakyReLU(0.2)(d2)
+        x = SpectralNormalization(layers.Conv2D(256, 4, strides=2, padding='same'))(x)
+        x = layers.LeakyReLU(0.2)(x)
         
-        # Bridge with attention
-        bridge = layers.Conv2D(256, 4, strides=2, padding='same')(d2)
-        bridge = layers.BatchNormalization()(bridge)
-        bridge = EdgeAttention()(bridge)
+        outputs = layers.Conv2D(1, 4, padding='same')(x)
         
-        # Decoder
-        u1 = layers.Conv2DTranspose(128, 4, strides=2, padding='same')(bridge)
-        u1 = layers.BatchNormalization()(u1)
-        u1 = layers.Concatenate()([u1, d2])
-        u1 = layers.ReLU()(u1)
-        
-        u2 = layers.Conv2DTranspose(64, 4, strides=2, padding='same')(u1)
-        u2 = layers.BatchNormalization()(u2)
-        u2 = layers.Concatenate()([u2, d1])
-        u2 = layers.ReLU()(u2)
-        
-        outputs = layers.Conv2D(1, 4, padding='same', activation='sigmoid')(u2)
-        
-        self.model = Model(inputs, outputs, name='Generator')
+        self.model = Model(inputs, outputs, name='Discriminator')
     
     def call(self, inputs):
         return self.model(inputs)
