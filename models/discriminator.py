ae16468b9757e0b9e97b6a131c0f3e786849f309
sijuswamyresearch <sijuswamy.research@gmail.com>
2025-04-01 02:26:26 +0530

    Update discriminator.py

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,41 +1,13 @@
-import tensorflow as tf
 from tensorflow.keras import layers, Model
-from .layers import EdgeAttention
+from .layers import SpectralNormalization
 
-class Generator(Model):
-    def __init__(self, config):
-        super().__init__()
-        self.config = config
-        self._build_model()
-
-    def _build_model(self):
-        inputs = layers.Input((self.config['model']['img_size'], self.config['model']['img_size'], self.config['model']['channels']))
-        
-        # Encoder
-        x = layers.Conv2D(64, 4, strides=2, padding='same')(inputs)
-        x = layers.LeakyReLU(0.2)(x)
-        
-        x = layers.Conv2D(128, 4, strides=2, padding='same')(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.LeakyReLU(0.2)(x)
-        
-        # Bridge
-        x = layers.Conv2D(256, 4, strides=2, padding='same')(x)
-        x = layers.BatchNormalization()(x)
-        x = EdgeAttention()(x)
-        
-        # Decoder
-        x = layers.Conv2DTranspose(128, 4, strides=2, padding='same')(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.ReLU()(x)
-        
-        x = layers.Conv2DTranspose(64, 4, strides=2, padding='same')(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.ReLU()(x)
-        
-        outputs = layers.Conv2D(1, 4, padding='same', activation='sigmoid')(x)
-        
-        self.model = Model(inputs, outputs, name='Generator')
-    
-    def call(self, inputs):
-        return self.model(inputs)
+def build_discriminator(img_size: int = 256) -> Model:
+    inputs = layers.Input((img_size, img_size, 1))
+    x = SpectralNormalization(layers.Conv2D(64, 4, strides=2, padding='same', kernel_initializer='he_uniform'))(inputs)
+    x = layers.LeakyReLU(0.2)(x)
+    x = SpectralNormalization(layers.Conv2D(128, 4, strides=2, padding='same', kernel_initializer='he_uniform'))(x)
+    x = layers.LeakyReLU(0.2)(x)
+    x = SpectralNormalization(layers.Conv2D(256, 4, strides=2, padding='same', kernel_initializer='he_uniform'))(x)
+    x = layers.LeakyReLU(0.2)(x)
+    outputs = layers.Conv2D(1, 4, padding='same', kernel_initializer='glorot_uniform')(x)
+    return Model(inputs, outputs, name='Discriminator')
